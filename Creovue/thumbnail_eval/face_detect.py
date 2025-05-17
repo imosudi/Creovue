dd8c9003440df7a7179e4b5830b8b87dd2886cef
Mosudi I. O. <imosudi@gmail.com>
2025-05-17 02:09:09 +0100

    Update requirements

--- Creovue/thumbnail_eval/face_detect.py
+++ Creovue/thumbnail_eval/face_detect.py
@@ -15,6 +15,7 @@ face_bp = Blueprint(
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static', 'uploads')
+UPLOAD_FOLDER = '/tmp/creovue_uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
