ab0dc719a242cbe43334b6721a15ba13d293036b
Mosudi I. O. <imosudi@gmail.com>
2025-05-21 21:11:53 +0100

    Analytics

--- Creovue/utils/yt_api.py
+++ Creovue/utils/yt_api.py
@@ -153,6 +153,7 @@ def calculate_ctr_metrics(channel_id, days=700):
     
     # Get channel_id from the current user
     channel_id = current_user.channel_id
+    print("channel_id: ", channel_id)
     if not channel_id:
         # If channel_id is not stored in the user model, fetch it using the credentials
         creds = Credentials(**session['youtube_token'])
