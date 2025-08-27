17d022d8dc5bde30fee8a55230b9325665bffce6
Mosudi I. O. <imosudi@gmail.com>
2025-08-27 23:27:32 +0200

    AG initial commit-27-082025

--- Creovue/routes.py
+++ Creovue/routes.py
@@ -43,13 +43,13 @@ from Creovue.utils.youtube_client import ensure_channel_id
 from Creovue.utils.yt_api import calculate_ctr_metrics
 
 print("Inside routes 4")
-from .competitors_analytics import *
+"""from .competitors_analytics import *
 from .alerts_notifications import *
 from .tools_utilities import *
 from .goals_progress import *
 from .content_planning_optimisation import *
 from .audience_insights import *
-from .seo_discovery_tools import * 
+from .seo_discovery_tools import *""" 
 print("Inside routes 5")
 from Creovue.helper_funcrions import analyse_channel_growth, analyse_videos_batch, get_channel_health_overview, get_detailed_video_analytics, get_health_recommendations, get_video_performance_data, get_video_performance_trends, predict_channel_growth
 from Creovue.utils.analytics_helpers import calculate_channel_health_score
